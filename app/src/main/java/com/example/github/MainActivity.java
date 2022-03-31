843d65e0c2ef8bc2dc431cc0394dd2cae13e155b
Carlos Jose Almeida <carlaopereirasanca@gmail.com>
2022-03-31 09:59:27 -0300

    insweridos comentários pra exemplificar como subir alterações no github

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Comentário inserido só pra ter uma alteração,
+    // alguma coisa pra enviar para o github.
+    // bla bla bla bla bla
+    // Instituto Federal de São Paulo
+    // São Carlos, SP, Brasil, América do Sul, Terra, Sistema Solar, Via Láctea
+    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
+
